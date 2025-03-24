8950b81fa609fb7c7c42cceabf665668497608e1
Rafael Paz <rafael_mourapaz@hotmail.com>
2025-03-24 09:13:35 -0300

    teste rafael

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Teste na branch secundaria-moises");
+        System.out.println("Agor foi essa parada");
     }
 }
